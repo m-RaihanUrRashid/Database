98d88068e78184fe2e092167587739b22d3206d4
m-RaihanUrRashid <151781099+m-RaihanUrRashid@users.noreply.github.com>
2023-11-24 23:45:16 +0600

    Updated .env

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,13 +4,30 @@
 <style>
   .square-box {
     max-width: 600px;
-    min-height: 500px;
+    min-height: 400px;
     background-color: white;
-}
+  }
+
+  .custom {
+    display: flex;
+  }
+
+  #bottom{
+    display: flex;     /* Create the flex container */
+    align-items: flex-end;  /* Align text to the bottom */
+  }
 </style>
 <div class= "container square-box d-flex justify-content-center align-items-center">
-  <img src="/img/image.png"  alt="cry">
   <form style = "width:500px; height:100px">
+  <div class = "custom">
+    <div> 
+      <img src="/img/image.png" width="120" height="100" alt="cry">
+    </div>
+    <div style= "padding: 0px 0px 0px 10px;"> 
+      <div id = "bottom"><h1 style = "font-family:'Serif' "> Mental Health Resources </h1></div>
+    </div>
+  </div>
+  <br>
     <div class="mb-3">
       <label for="exampleInputEmail1" class="form-label">Email address</label>
       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
@@ -22,7 +39,7 @@
     </div>
     <div class="mb-3 form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1">
-      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+      <label class="form-check-label" for="exampleCheck1">Remember me</label>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>
---
+++ resources/views/patientHome.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('title' , 'Home')
+@section('content')
+<h1>Patient Home</h1>
+@endsection
