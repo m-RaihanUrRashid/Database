064858ce5a18502ded32f49de82c26a22e76b5f9
nazifasaurus <139798305+nazifasaurus@users.noreply.github.com>
2023-11-25 01:20:28 +0600

    Made login pretty
    changed kalar

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,7 +4,7 @@
 <style>
   .square-box {
     max-width: 600px;
-    min-height: 400px;
+    min-height: 300px;
     background-color: white;
   }
 
@@ -34,14 +34,17 @@
       <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
     </div>
     <div class="mb-3">
+    <br>
       <label for="exampleInputPassword1" class="form-label">Password</label>
+      
       <input type="password" class="form-control" id="exampleInputPassword1">
     </div>
     <div class="mb-3 form-check">
       <input type="checkbox" class="form-check-input" id="exampleCheck1">
       <label class="form-check-label" for="exampleCheck1">Remember me</label>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
+    <button type="submit" class="btn btn-primary" style=background-color:cadetblue;>Login</button>
+
   </form>
 </div>
 @endsection
