0bcd1eed5fefae90c599c430a36a933f708e0f37
Gazi Muhammad Mobasher <gazimobassher@gmail.com>
2023-11-29 20:27:40 +0600

    Did some work

--- resources/views/therapistHome.blade.php
+++ resources/views/therapistHome.blade.php
@@ -1,13 +1,79 @@
 @extends('layout')
-@section('title' , 'Therapist Home')
+@section('title' , 'Therapist Appointment Chart')
 @section('content')
 
 <head>
-    <style>h1, h2, p{color: darkblue; font-family: "Helvetica";}</style>
+    <style>h1, h2, p{color: darkblue; font-family: "Georgia";}</style>
 </head>
 
 <body>
-<h1 style="text-align: center;">Therapist Home</h1> <br> <br> <br>
+<!-- Navbar aesthetic properties -->
+<style>
+    .navbar {
+    background-color: navy;
+    padding: 15px;
+    color: white;
+    text-align: center;
+    transition: 0.3s; /* Add smooth transition effect */
+    overflow: hidden;
+    }
+
+    .navbar a{
+        font-family: 'Georgia';
+        color: white;
+        padding: 5px;
+        margin: 5px;
+    }
+    .navbar a:hover{
+        color: blue!important; cursor: pointer!important; transition: 0.2s!important;
+    }
+    .navb{
+        width: 2px; /* Adjust border width */
+        height: 100%; /* Adjust border height */
+        background-color: black; /* Adjust border color */
+    }
+</style>
+
+
+<!-- Navbar script -->
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    const navbar = document.getElementById('navbar');
+
+    document.addEventListener('mousemove', function(e) {
+        // Check if the cursor is outside the navbar
+        if (!navbar.contains(e.target)) {
+            navbar.style.height = '0';
+        } else {
+            navbar.style.height = '70px';
+        }
+        });
+    });
+</script>
+
+<!-- Navbar -->
+<div class="navbar" id="navbar">
+        <a style= "margin-left: 20px">SALVATIUM</a>
+        <span class="navb"></span>
+        <section style= "display: flex;">
+            <div style= "padding-top: 6.5px;"> <img src="/img/profile logo inv.png" width="20" height="20" alt="pl"></div>
+            <a>Profile</a> 
+        </section>
+        <span class="navb"></span>
+        <section style= "display: flex;">
+            <div style= "padding-top: 6.5px;"> <img src="/img/notebook.png" width="30" height="30" alt="nb"></div>
+            <a>Notes</a> 
+        </section>
+        <span class="navb"></span>
+        <section style= "display: flex;">
+            <div style= "padding-top: 6.5px;"> <img src="/img/lines.png" width="15" height="15" alt="nb"></div>
+            <a>More</a> 
+        </section>
+</div>
+
+<div style="padding: 20px"></div>
+
+<h1 style="text-align: center;">Appointment Section</h1> <br> <br> <br>
 
 <section style="display: flex; margin-bottom: 5%">
     <p style="font-size: 1.5em; padding-left: 170px; margin-bottom: 0%">Appointment Chart</p>
@@ -23,6 +89,7 @@
         border-collapse: collapse; /* Collapse the borders */
         margin: 20px; /* Add margin for spacing */
         margin: 0 auto;
+        margin-bottom: 80px;
     }
     th, td{
         border: 1.5px solid darkblue;
@@ -125,6 +192,10 @@
         transition: 0.2s!important;
     }
 </style>
+
+
+
+
 
 
 
