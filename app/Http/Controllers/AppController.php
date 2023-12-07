41673760405edde33dd40b4079be70a93e891c21
ikramakif42 <ikramakif42@gmail.com>
2023-12-08 02:25:28 +0600

    Revert "Update AppController.php"
    This reverts commit 1e6ec1383eea50d5e965c4a53b8e372674743667.

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -21,30 +21,19 @@ class AppController extends Controller
     }
 
     public function togle(Request $request)
-{       
-        $requestData = json_decode($request->input('markasdone'), true);
+{        
 
-        // Check if decoding was successful
-        if ($requestData) {
-            $cpUserID = $requestData['cpUserID'];
-            $csUserID = $requestData['csUserID'];
-            $dappDate = $requestData['dappDate'];
-            $dappTime = $requestData['dappTime'];
-            $cappStatus = $requestData['cappStatus'];
+        $appointment = $request;
 
-            $appointment = Appointment::where('cpUserID', $cpUserID)
-            ->where('csUserID', $csUserID)
-            ->where('dappDate', $dappDate)
-            ->where('dappTime', $dappTime)
-            ->first();
-            
-            $appointment->cappStatus = 'yes';
-            $appointment->save();
+        $appointment = Appointment::where('cpUserID', $request->cpUserID)
+        ->where('csUserID', $request->csUserID)
+        ->where('dappDate', $request->dappDate)
+        ->where('dappTime', $request->dappTime)
+        ->first();
+        
+        $appointment->cappStatus = 'yes';
+        $appointment->save();
 
-            return redirect()->route('psychAppt.view'); // Assuming 'psychAppt.view' is the route to your view method
-
-        } else {
-            dd("error");
-        }
+        return redirect()->route('psychAppt.view'); // Assuming 'psychAppt.view' is the route to your view method
 }
 }
